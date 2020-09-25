07f6a1a058dc315e0c6d9e19c833519710fbbd04
Derek Worthen <deworthe@microsoft.com>
2020-09-25 07:19:11 -0700

    refactor: bundling and deployment

--- packages/server/src/schema.ts
+++ packages/server/src/schema.ts
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { readFileSync } from 'fs'
 
-const schemaFileName = require.resolve('@newsthreads/schema/schema.gql')
-export const schemaText = readFileSync(schemaFileName, { encoding: 'utf8' })
+/* eslint-disable-next-line @typescript-eslint/no-var-requires */
+export const schemaText = require('@newsthreads/schema/schema.gql')
